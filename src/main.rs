b5c1cf61182617845c41e65f4b4a6a55442a741f
faineance <faineance@users.noreply.github.com>
2015-06-10 15:23:18 +0100

    begin main fn

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 #![feature(convert)] 
 mod vm;
 use vm::{VM, Instruction};
-
+use std::env;
+use std::io::prelude::*;
+use std::fs::File;
 static USAGE: &'static str = "
 Usage:
-vmach assemble 
+vmach repl
 vmach run
 vmach -h | --help
 vmach --version
@@ -16,9 +18,37 @@ Options:
 
 
 fn main() {
-	let mut vm = VM::new();
+	let mut arguments = env::args();
+	arguments.next();
+	match arguments.next() {
+		Some(command) => {
+			match command.as_ref() {
+				"repl" => {
 
-	let program = vec![Instruction::PSH(6),Instruction::PSH(7),Instruction::ADD, Instruction::OUT, Instruction::HLT];	
-	vm.run(program);
-	// println!("{:?}", vm);
+				},
+				"run" => {
+					match arguments.next() {
+						Some(filename) => {
+							let mut s = String::new(); 
+							match File::open(filename) {
+								Ok(mut input) => {
+									// let mut program = String::new(); 
+
+									// input.read_to_string(&mut program);
+
+									let mut vm = VM::new();
+									let program = vec![Instruction::PSH(6),Instruction::PSH(7),Instruction::ADD, Instruction::OUT, Instruction::HLT];	
+									vm.run(program);
+								},
+								Err(error) => panic!("{}", error),
+							}
+						}
+						_ => println!("No file specified\n{}", USAGE),
+					}
+				}
+				_ => println!("Unrecognised command \n{}", USAGE),
+			}
+		},
+		_ => println!("No command specified \n{}", USAGE),
+	}
 }
