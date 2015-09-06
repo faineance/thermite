4557af1a91a95f57a9d53d08f5649f7c85c45bd9
faineance <faineance@users.noreply.github.com>
2015-09-06 14:00:30 +0100

    simplify parsing

--- src/lib/parser.rs
+++ src/lib/parser.rs
@@ -8,16 +8,8 @@ pub type ParserResult<T> = Result<Option<T>, ParserError>;
 pub fn parse(input: Vec<Token>) -> Vec<Instruction> {
 	let mut parser = Parser::new(input);
 	let mut program = vec![];
-
-	loop {
-		match parser.next() {
-			Some(t) => {
-
-				program.push(t);
-
-			}
-			None => break
-		}
+	while let Some(i) = parser.next() {
+		program.push(i)
 	}
 	program
 }
@@ -43,42 +35,40 @@ impl Parser {
 		}
 		None
 	}
+	fn take_register(&mut self) -> Option<Register> {
+		match self.advance().unwrap() {
+			Token::Identifier(r) => Some(Register::from(r)),
+			_ => return None
+		}
+	}
+	fn take_value(&mut self) -> Option<i32> {
+		match self.advance().unwrap() {
+			Token::Value(i) => Some(i),
+			_ => return None
+		}
+	}
+
 	fn handle_io(&mut self, kind: IOType) -> ParserResult<Instruction> {
-		let register = match self.advance().unwrap() {
-			Token::Identifier(r) => Register::from(r),
-			_ => return Err(ParserError::InvalidArgument)
-		};
-		Ok(Some(Instruction::IO(kind, register)))
+		match self.take_register() {
+			Some(register) => Ok(Some(Instruction::IO(kind, register))),
+			None => return Err(ParserError::InvalidArgument)
+		}
 	}
 	fn handle_arithmatic(&mut self, kind: ArithmaticType) -> ParserResult<Instruction> {
-		let source = match self.advance().unwrap() {
-			Token::Identifier(r) => Register::from(r),
+		match (self.take_register(), self.take_register(), self.take_register()) {
+			(Some(source), Some(target), Some(destination)) => {
+				Ok(Some(Instruction::Arithmatic(kind, source, target, destination)))
+			},
 			_ => return Err(ParserError::InvalidArgument)
-		};
-		let target = match self.advance().unwrap() {
-			Token::Identifier(r) => Register::from(r),
-			_ => return Err(ParserError::InvalidArgument)
-		};
-		let destination = match self.advance().unwrap() {
-			Token::Identifier(r) => Register::from(r),
-			_ => return Err(ParserError::InvalidArgument)
-		};
-		Ok(Some(Instruction::Arithmatic(kind, source, target, destination)))
+		}
 	}
 	fn handle_bitwise(&mut self, kind: BitwiseType) -> ParserResult<Instruction> {
-		let source = match self.advance().unwrap() {
-			Token::Identifier(r) => Register::from(r),
+		match (self.take_register(), self.take_register(), self.take_register()) {
+			(Some(source), Some(target), Some(destination)) => {
+				Ok(Some(Instruction::Bitwise(kind, source, target, destination)))
+			},
 			_ => return Err(ParserError::InvalidArgument)
-		};
-		let target = match self.advance().unwrap() {
-			Token::Identifier(r) => Register::from(r),
-			_ => return Err(ParserError::InvalidArgument)
-		};
-		let destination = match self.advance().unwrap() {
-			Token::Identifier(r) => Register::from(r),
-			_ => return Err(ParserError::InvalidArgument)
-		};
-		Ok(Some(Instruction::Bitwise(kind, source, target, destination)))
+		}
 	}
 	fn handle_branch(&mut self, kind: BranchType) -> ParserResult<Instruction> {
 		let label = match self.advance().unwrap() {
@@ -91,96 +81,78 @@ impl Parser {
 		Ok(Some(Instruction::Control(kind)))
 	}
 	fn handle_assignment(&mut self, kind: AssignmentType) -> ParserResult<Instruction> {
-		let register = match self.advance().unwrap() {
-			Token::Identifier(r) => Register::from(r),
-			_ => return Err(ParserError::InvalidArgument)
-		};
-		Ok(Some(Instruction::Assignment(kind, register)))
-	}
-
-	fn next_instruction(&mut self) -> ParserResult<Instruction> {
-		match self.advance() {
-			Some(t) => {
-				match t {
-					Token::Identifier(mut i) => {
-						match i.as_ref() {
-
-							"nop" => Ok(Some(Instruction::NOP)),
-							"hlt" => Ok(Some(Instruction::HLT)),
-
-							"out" => self.handle_io(IOType::OUT),
-							"in" => self.handle_io(IOType::IN),
-
-							"add" => self.handle_arithmatic(ArithmaticType::ADD),
-							"sub" => self.handle_arithmatic(ArithmaticType::SUB),
-							"mul" => self.handle_arithmatic(ArithmaticType::MUL),
-							"div" => self.handle_arithmatic(ArithmaticType::DIV),
-							"max" => self.handle_arithmatic(ArithmaticType::MAX),
-							"min" => self.handle_arithmatic(ArithmaticType::MIN),
-
-							"and" => self.handle_bitwise(BitwiseType::AND),
-							"or"  => self.handle_bitwise(BitwiseType::OR),
-							"xor" => self.handle_bitwise(BitwiseType::XOR),
-							"shr" => self.handle_bitwise(BitwiseType::SHIFT(ShiftType::RIGHT)),
-							"shl" => self.handle_bitwise(BitwiseType::SHIFT(ShiftType::LEFT)),
-
-							"jmp" => self.handle_branch(BranchType::UNCONDITIONAL),
-							"jz" => {
-								let register = match self.advance().unwrap() {
-									Token::Identifier(r) => Register::from(r),
-									_ => return Err(ParserError::InvalidArgument)
-								};
-								self.handle_branch(BranchType::ZERO(register))
-							}
-							"jnz" => {
-								let register = match self.advance().unwrap() {
-									Token::Identifier(r) => Register::from(r),
-									_ => return Err(ParserError::InvalidArgument)
-								};
-								self.handle_branch(BranchType::NOTZERO(register))
-							},
-
-							"str" => {
-								let value = match self.advance().unwrap() {
-									Token::Value(i) => i,
-									_ => return Err(ParserError::InvalidArgument)
-								};
-								self.handle_assignment(AssignmentType::STR(value))
-							},
-							"cpy" => {
-								let register = match self.advance().unwrap() {
-									Token::Identifier(r) => Register::from(r),
-									_ => return Err(ParserError::InvalidArgument)
-								};
-								self.handle_assignment(AssignmentType::CPY(register))
-							},
-							_ if i.chars().last().unwrap() == ':' => {
-								i.pop().unwrap();
-								self.handle_control(ControlType::LBL(i))
-							},
-
-							_ => return Err(ParserError::InvalidInstruction)
-						}
-
-					},
-					_ => return Err(ParserError::InvalidInstruction),
-				}
-
-			}
-			None => return Ok(None)
+		match self.take_register() {
+			Some(register) => Ok(Some(Instruction::Assignment(kind, register))),
+			None => return Err(ParserError::InvalidArgument)
 		}
+	}
+	fn next_instruction(&mut self) -> ParserResult<Instruction> {
+		let mut result = Ok(None);
+		if let Some(t) = self.advance() {
+			if let Token::Identifier(mut i) = t {
+				match i.as_ref() {
+					"nop" => result = Ok(Some(Instruction::NOP)),
+					"hlt" => result = Ok(Some(Instruction::HLT)),
 
+					"out" => result = self.handle_io(IOType::OUT),
+					"in" => result =  self.handle_io(IOType::IN),
+
+					"add" => result = self.handle_arithmatic(ArithmaticType::ADD),
+					"sub" => result = self.handle_arithmatic(ArithmaticType::SUB),
+					"mul" => result = self.handle_arithmatic(ArithmaticType::MUL),
+					"div" => result = self.handle_arithmatic(ArithmaticType::DIV),
+					"max" => result = self.handle_arithmatic(ArithmaticType::MAX),
+					"min" => result = self.handle_arithmatic(ArithmaticType::MIN),
+
+					"and" => result = self.handle_bitwise(BitwiseType::AND),
+					"or"  => result = self.handle_bitwise(BitwiseType::OR),
+					"xor" => result = self.handle_bitwise(BitwiseType::XOR),
+					"shr" => result = self.handle_bitwise(BitwiseType::SHIFT(ShiftType::RIGHT)),
+					"shl" => result = self.handle_bitwise(BitwiseType::SHIFT(ShiftType::LEFT)),
+
+					"jmp" => result = self.handle_branch(BranchType::UNCONDITIONAL),
+					"jz" => {
+						match self.take_register() {
+							Some(register) => result = self.handle_branch(BranchType::ZERO(register)),
+							None => return Err(ParserError::InvalidArgument)
+						}
+					},
+					"jnz" => {
+						match self.take_register() {
+							Some(register) => result = self.handle_branch(BranchType::NOTZERO(register)),
+							None => return Err(ParserError::InvalidArgument)
+						}
+					},
+					"str" => {
+						match self.take_value() {
+							Some(value) => result = self.handle_assignment(AssignmentType::STR(value)),
+							None => return Err(ParserError::InvalidArgument)
+						}
+					},
+					"cpy" => {
+						match self.take_register() {
+							Some(register) => result = self.handle_assignment(AssignmentType::CPY(register)),
+							None => return Err(ParserError::InvalidArgument)
+						}
+					},
+					_ if i.chars().last().unwrap() == ':' => {
+						i.pop().unwrap();
+						result = self.handle_control(ControlType::LBL(i))
+					},
+					_ => return Err(ParserError::InvalidInstruction)
+				}
+			}
+		}
+		result
 	}
 }
 impl Iterator for Parser {
 	type Item = Instruction;
 	fn next(&mut self) -> Option<Instruction> {
-
 		match self.next_instruction() {
 			Ok(i) => i,
 			Err(e) => panic!("ParserError: {:?}", e)
 		}
-
 	}
 }
 
